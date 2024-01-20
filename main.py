10c02bbce76c66983c85b560bcad69d34c01a50b
peter andrew <peter.andrew987@gmail.com>
2024-01-20 21:43:56 +0700

    add user choose mechanism

--- main.py
+++ main.py
@@ -12,10 +12,21 @@ def select_random_pokemon():
     ]
     return random.choice(pokemon_list)
 
+def ask_user_choose():
+    pokemon_list = [
+        Bulbasaur(),
+        Charmander(),
+        Squirtle(),
+    ]
+    chose_pokemon_prompt = """1. Bulbasaur\n2. Charmender\n3. Squirtle\nPlease choose your pokemon (1/2/3):"""
+    user_choice = int(input(chose_pokemon_prompt))
+    return pokemon_list[user_choice-1]
+
 
 
 computer = select_random_pokemon()
-
+user = ask_user_choose()
 print(computer)
+print(user)
 
 
